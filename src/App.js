aa4b75554a6dda1c08ef99f11827512ac3d90578
Theo Sardin <theosardin@Theos-MacBook-Pro.local>
2023-03-20 21:27:43 +0100

    initial stuff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { Avatar } from '@mui/material';
+import logo from './assets/logo-societe-generale.png'; 
+import CardMedia from '@mui/material/CardMedia';
+import CvItem from './CvItem';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <div>
+      <CvItem
+      from={"Oct. 2022"}
+      to={"Present"} title="Software engineer"
+      description="Fullstack Front office developer in FOLAB team in Societe Generale"
+      logo={logo}
+      bulletPoints={["hey", "you"]} />
+      </div>
+      <div>
+        <CvItem
+        from={"Apr. 2019"}
+        to={"Oct. 2022"} 
+        title="Software Engineer"
+        description="Fullstack software engineer in QDR team in Societe Generale"
+        logo={logo}
+        bulletPoints={[
+          "Day to day development and support directly for traders on trading floor", 
+          "Algorithm trading desk tactical IT: sets of microservices to enable the business such as risk and p&L reporting, news notification or exposing historical data for analysis", 
+          "Listed products desk: Developing a tool for live monitoring of the markets, analyze competition behaviour and propose parameters adjustment. Leveraging Akka framework and event based programming", 
+          "Creating a big data platform: in-house leveraging spark, kubernetes and zeppelin notebooks, now moving to Microsoft Azure cloud, working with data analysts to enable new possibilities for business", 
+          "Working with stakeholders (traders, data analysts, IT counterparts): handling client relationships, gathering functional requirements and driving technical choices"]} />
+        </div>
+      </>
+)
 }
-
-export default App;
---
+++ src/CvItem.jsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { Avatar } from '@mui/material';
+//import logo from './assets/logo-societe-generale.png'; 
+import CardMedia from '@mui/material/CardMedia';
+
+export default function CvItem({from, to, title, description, logo, bulletPoints}){
+    return (
+      <Card sx={{ maxWidth: 700, width: 700, marginBottom: 5 }}>
+          <CardContent>
+            <Typography sx={{ fontSize: 14 }} color="text.primary" gutterBottom>
+              {from} - {to}
+            </Typography>
+            <Typography variant="h5" component="div">
+              {title}
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+              {description}
+            </Typography>
+          <ul>
+            {bulletPoints.map(el=><li><Typography>{el}</Typography></li>)}
+          </ul>
+          </CardContent>
+
+        </Card>
+      );
+}
+

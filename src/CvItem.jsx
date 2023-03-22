68db5417f0e8b9f4c6c8cd8c39bd1467454756cd
Theo Sardin <theosardin@Theos-MacBook-Pro.local>
2023-03-22 12:11:36 +0100

    fix image size

--- src/CvItem.jsx
+++ src/CvItem.jsx
@@ -17,21 +17,22 @@ export default function CvItem({from, to, title, description, logo, bulletPoints
       height: 140,
       borderRadius: "4px",
       position: "absolute",
-      marginLeft: matches ? 105 : 40,
+      marginLeft: matches ? 105 : 30,
+
       zIndex: 1000
     }
+    
   }
-
+  
 
   const width = matches ? 1000 : 365;
   const fontSize = matches ? 18 : 15;
+  const imgSize = matches ? '100px' :  '50px';
     return (
       <Card sx={{ maxWidth: width, width: width, marginBottom: 5}}>
           <CardContent>
-          <CardMedia
-        sx={styles.media}
-      >
-        <img src={logo} style={{width: 100, height:100, alignSelf: 'right'}} />
+          <CardMedia sx={styles.media}>
+            <img src={logo} style={{width: imgSize, height:imgSize}}/>
       </CardMedia>
             <Typography sx={{ fontSize: fontSize }} color="text.primary" gutterBottom>
               {from} - {to} | {location}

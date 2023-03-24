6322c342d862eb794688869e3920f09b6cb08e72
theo-sardin <sardin.theo@gmail.com>
2023-03-24 22:56:00 +0100

    add backend

--- src/App.js
+++ src/App.js
@@ -12,11 +12,13 @@ import logoTse from './assets/logo-tse.png';
 import CvAppBar from './CvAppBar.jsx';
 import CvItem from './CvItem';
 import SkillItem from './SkillsItem.jsx';
+import { useEffect } from 'react';
 
 
 function TabPanel(props) {
 
   const { children, value, index, ...other } = props;
+
 
   return (
     <div
@@ -35,13 +37,38 @@ function TabPanel(props) {
     </div>
   );
 }
-export default function App() {
-  const [value, setValue] = React.useState(0);
 
+export default function App() {
+  const logos = {
+    logoSg: logoSg,
+    logoBva: logoBva,
+    logoDauphine: logoDauphine,
+    logoTse: logoTse
+  }
+  const [value, setValue] = React.useState(0);
+  const[workResumeItems, setWorkItems] = React.useState([{from: "", to: "", title: "", description: "", location:"", logoId:"", bulletPoints: []}]);
+  const[educationResumeItems, setEducationItems] = React.useState([{from: "", to: "", title: "", description: "", location:"", logoId:"", bulletPoints: []}]);
+
+  useEffect(()=>{fetch('http://theocv-backend.eu-west-3.elasticbeanstalk.com/resume/data/work', {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    }
+  }).then(resp => resp.json().then(x=>setWorkItems(x)))},[]);
+
+  useEffect(()=>{fetch('http://theocv-backend.eu-west-3.elasticbeanstalk.com/resume/data/education', {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    }
+  }).then(resp => resp.json().then(x=>setEducationItems(x)))}, []);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+  
   return (
 
     <>
@@ -54,152 +81,77 @@ export default function App() {
         </Tabs>
       </Box>
       <TabPanel value={value} index={0}>
-        <div>
-          <CvItem
-            from={"Oct. 2022"}
-            to={"Present"} 
-            title="Software engineer - Associate"
-            description="Fullstack Front office developer in FOLAB team in Societe Generale"
-            logo={logoSg}
-            location="Paris, France"
-            bulletPoints={[
-              "<b>FOLAB team:</b> Delivery and time to market oriented team - promotes flexibility, adaptability and fast learning",
-              "<b>Working closely with clients</b> by being localized with them</b> ",
-              "<b>Listed products desk:</b> Build trading tools to monitor trades live, explain model behaviours, and monitor competition",
-              "<b>Maintain, support and enhance</b> a vast array of existing tools and projects"
+        {workResumeItems.map(item => 
+          <div>
+            <CvItem
+              from={item.from}
+              to={item.to}
+              title={item.title}
+              description={item.description}
+              logo={logos[item.logoId]}
+              location={item.location}
+              bulletPoints={item.bulletPoints} />
+          </div>
+        )}
 
-            ]} />
-        </div>
-        <div>
-          <CvItem
-            from={"Apr. 2019"}
-            to={"Oct. 2022"}
-            title="Software Engineer - Associate"
-            description="Fullstack software engineer in QDR team in Societe Generale"
-            logo={logoSg}
-            location="Hong Kong"
-            bulletPoints={[
-              "<b>Day to day development</b> and support directly for traders on trading floor",
-              "<b>Algorithm trading desk tactical IT:</b> sets of microservices to enable the business such as risk and p&L reporting, news notification or exposing historical data for analysis",
-              "<b>Listed products desk:</b> Developing a tool for live monitoring of the markets, analyze competition behaviour and propose parameters adjustment. Leveraging Akka framework and event based programming",
-              "<b>Creating a big data platform:</b> in-house leveraging spark, kubernetes and zeppelin notebooks, now moving to Microsoft Azure cloud, working with data analysts to enable new possibilities for business",
-              "<b>Working with stakeholders (traders, data analysts, IT counterparts):</b> handling client relationships, gathering functional requirements and driving technical choices"]} />
-        </div>
-        <div>
-          <CvItem
-            from={"Feb. 2018"}
-            to={"Apr. 2019"} title="VIE - IT COO team"
-            description="Management control team"
-            logo={logoSg}
-            location="Hong Kong"
-            bulletPoints={[
-              "<b>Budget follow up</b>, creating monthly reports to asses IT teams budget consumption and assist managers in forecasting accordingly",
-              "<b>Updating purchase processes</b> to improve quality of vendor costs analysis and budgeting",
-              "<b>Working with managers to evaluate staffing needs</b> and ensure data quality of the financial reporting",
-              "<b>Automating daily processes</b>",
-              "<b>Organizing and managing town-hall events</b>"]} />
-        </div>
-        <div>
-          <CvItem
-            from={"Feb. 2018"}
-            to={"Apr. 2019"} title="Internship - Business Analyst"
-            description="Management control team"
-            logo={logoSg}
-            location="Paris, France"
-            bulletPoints={["<b>Business analyst</b> for the development of a digital platform in ITEC/COO team",
-              "Gathering requirements, writing functional specifications and conduct user testing"]} />
-        </div>
-        <div>
-          <CvItem
-            from={"Jun. 2016"}
-            to={"Aug. 2016"} title="Internship - BVA"
-            description="Business Intelligence developer"
-            logo={logoBva}
-            location="Hong Kong"
-            bulletPoints={[
-              "<b>Developing a tool to automate invoice processing</b> and cost analysis",
-              "Backend using php and mySQL",
-              "<b>PowerBI</b> for dashboarding and analysis"]} />
-        </div>
       </TabPanel>
       <TabPanel value={value} index={1}>
-        <div>
-          <CvItem
-            from={"2015"}
-            to={"2017"}
-            title="Paris Dauphine University"
-            description="MASTERS DEGREE IN FINANCE AND INFORMATION TECHNOLOGY"
-            location={"Paris, France"}
-            logo={logoDauphine}
-            bulletPoints={[]}
-          />
-        </div>
-        <div>
-          <CvItem
-          
-            from={"2014"}
-            to={"2015"}
-            location="Paris, France"
-            title="Paris Dauphine University"
-            description="BACHELOR IN INFORMATION TECHNOLOGY APPLIED TO MANAGEMENT"
-            logo={logoDauphine}
-            bulletPoints={[]}
-          />
-
-          <CvItem
-            from={"2012"}
-            to={"2014"}
-            location="Toulouse, France"
-            title="Toulouse School of Economics"
-            description="PREPARATORY CLASSES IN COMPUTER SCIENCE AND ECONOMICS"
-            logo={logoTse}
-            bulletPoints={[]}
-          />
-        </div>
+      {educationResumeItems.map(item => 
+          <div>
+            <CvItem
+              from={item.from}
+              to={item.to}
+              title={item.title}
+              description={item.description}
+              logo={logos[item.logoId]}
+              location={item.location}
+              bulletPoints={item.bulletPoints} />
+          </div>
+        )}
       </TabPanel>
-       
+
       <TabPanel value={value} index={2}>
         <div>
           <SkillItem
             category={'Development languages'}
             listSkills=
-              {[
-                {'name': 'Java', 'rating': 4},
-                {'name': 'Python', 'rating': 3},
-                {'name': 'Typescript', 'rating': 3},
-                {'name': 'Javascript', 'rating': 2},
-                {'name': 'C#', 'rating': 1}
+            {[
+              { 'name': 'Java', 'rating': 4 },
+              { 'name': 'Python', 'rating': 3 },
+              { 'name': 'Typescript', 'rating': 3 },
+              { 'name': 'Javascript', 'rating': 2 },
+              { 'name': 'C#', 'rating': 1 }
 
-              ]}
-            
+            ]}
+
           />
           <SkillItem
             category={'Technologies and Frameworks'}
             listSkills=
-              {[
-                {'name': 'Springboot', 'rating': 4},
-                {'name': 'Spark', 'rating': 2},
-                {'name': 'Kubernetes', 'rating': 3},
-                {'name': 'PostgreSQL', 'rating': 3},
-                {'name': 'Angular', 'rating': 3},
-                {'name': 'React', 'rating': 1},
-                {'name': 'Windows', 'rating': 2},
-                {'name': 'Unix', 'rating': 3}
-              ]}
-            
+            {[
+              { 'name': 'Springboot', 'rating': 4 },
+              { 'name': 'Spark', 'rating': 2 },
+              { 'name': 'Kubernetes', 'rating': 3 },
+              { 'name': 'PostgreSQL', 'rating': 3 },
+              { 'name': 'Angular', 'rating': 3 },
+              { 'name': 'React', 'rating': 1 },
+              { 'name': 'Windows', 'rating': 2 },
+              { 'name': 'Unix', 'rating': 3 }
+            ]}
+
           />
-            <SkillItem
+          <SkillItem
             category={'Languages and misc'}
             listSkills=
-              {[
-                {'name': 'English', 'rating': 4},
-                {'name': 'French', 'rating': 5},
-                {'name': 'Italian', 'rating': 2},
-                {'name': 'Spanish', 'rating': 1},
-                {'name': 'Guitar', 'rating': 3},
-                {'name': 'Skiing', 'rating': 3},
-              ]}
-            
+            {[
+              { 'name': 'English', 'rating': 4 },
+              { 'name': 'French', 'rating': 5 },
+              { 'name': 'Italian', 'rating': 2 },
+              { 'name': 'Spanish', 'rating': 1 },
+              { 'name': 'Guitar', 'rating': 3 },
+              { 'name': 'Skiing', 'rating': 3 },
+            ]}
+
           />
         </div>
       </TabPanel>
--- src/CvItem.jsx
+++ src/CvItem.jsx
@@ -8,48 +8,41 @@ import { CardMedia } from '@mui/material';
 import Box from '@mui/material/Box';
 
 
-export default function CvItem({from, to, title, description, logo, bulletPoints, location}){
+export default function CvItem({ from, to, title, description, logo, bulletPoints, location }) {
   const mediumscreen = useMediaQuery('(min-width:600px)');
   const bigscreen = useMediaQuery('(min-width:1200px)');
 
-  const styles = {
-    media: {
-      height: 140,
-      borderRadius: "4px",
-      position: "absolute",
-      marginLeft: bigscreen ? 160 : mediumscreen ? 70 :  35,
 
-      zIndex: 1000
-    }
-    
-  }
-  
 
   const cardWidth = bigscreen ? 1500 : mediumscreen ? 1200 : 365;
   const fontSize = bigscreen ? 18 : 15;
-  const imgSize = bigscreen ? '100px' : mediumscreen ? '75px' : '50px';
-    return (
-      <Box sx={{width: '97vw', display: 'flex',justifyContent: 'center'}}>
-      <Card sx={{ maxWidth: cardWidth, width: cardWidth, marginBottom: 1}}>
-          <CardContent>
-          <CardMedia sx={styles.media}>
-            <img src={logo} style={{width: imgSize, height:imgSize}}/>
-      </CardMedia>
+  const imgSize = bigscreen ? '100px' : mediumscreen ? '75px' : '30px';
+  return (
+    <Box sx={{ width: '97vw', display: 'flex', justifyContent: 'center' }}>
+      <Card sx={{ maxWidth: cardWidth, width: cardWidth, marginBottom: 1 }}>
+        <CardContent>
+          <div style={{ display: 'flex', flexDirection: 'row' }}>
             <Typography sx={{ fontSize: fontSize }} color="text.primary" gutterBottom>
               <i>{from} - {to} | {location}</i>
             </Typography>
-            <Typography variant="h5" component="div">
-              {title}
-            </Typography>
-            <Typography sx={{ mb: 1.5 }} color="text.secondary">
-              {description}
-            </Typography>
+            <div style={{ flexGrow: 1 }}></div>
+            <CardMedia sx={{marginTop: '-2%'}}>
+              <img src={logo} style={{ width: imgSize, height: imgSize }} />
+            </CardMedia>
+          </div>
+
+          <Typography variant="h5" component="div" style={{marginTop: '-3%'}}>
+            {title}
+          </Typography>
+          <Typography sx={{ mb: 1.5 }} color="text.secondary">
+            {description}
+          </Typography>
           <ul >
-            {bulletPoints.map(el=><li dangerouslySetInnerHTML={{__html: el}}></li>)}
+            {bulletPoints.map(el => <li dangerouslySetInnerHTML={{ __html: el }}></li>)}
           </ul>
-          </CardContent>
-        </Card>
-        </Box>
-      );
+        </CardContent>
+      </Card>
+    </Box>
+  );
 }
 
